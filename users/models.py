9ff75018d8b1c6b31562e07641a90d15845aa75d
GloryKO <koladeg2022@gmail.com>
2023-04-11 09:59:09 -0700

    setup custom user, courses ,subjects and modules model

---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True,max_length=255)
+    name = models.CharField(max_length=200)
+    
+    USERNAME_FIELD ='email'
+    REQUIRED_FIELDS = ['name']
+
+    objects = CustomUserManager()
+    
+    def __str__(self):
+        return self.name
