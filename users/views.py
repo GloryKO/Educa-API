f625c77c51ca82a12e39ee42f87cae8f4de8ce15
GloryKO <koladeg2022@gmail.com>
2023-05-11 15:18:48 -0700

    added user update endpoint and tests

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics,authentication,permissions
 # Create your views here.
 from .serializers import UserSerializer,AuthTokenSerializer
 from rest_framework.settings import api_settings
@@ -13,4 +13,11 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user """
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
     
+    def get_object(self):
+        return self.request.user
